b273ca7529fcfea2a9ca879e0fbd20a503bdac77
Han <hanafi4399@gmail.com>
2023-09-17 19:23:01 +0700

    Complete part 1 practices

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,31 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+const svg = d3.select('#chart-area')
+  .append('svg')
+  .attr('width', 500)
+  .attr('height', 500)
+            
+const circle = svg.append('circle')
+  .attr('cx', 50)
+  .attr('cy', 50)
+  .attr('r', 50)
+  .attr('fill', 'blue')
+
+const line = svg.append('line')
+  .attr('x1', 200)
+  .attr('y1', 200)
+  .attr('x2', 300)
+  .attr('y2', 250)
+  .attr('stroke', 'red')
+  .attr('stroke-width', 2)
+
+const ellipse = svg.append('ellipse')
+  .attr('cx', 150)
+  .attr('cy', 150)
+  .attr('rx', 50)
+  .attr('ry', 100)
+  .attr('stroke', 'purple')
+  .attr('fill', 'white')
+
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,28 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json('./data/buildings.json').then((data) => {
+  console.log(data)
+
+  data.forEach((d) => {
+    d.height = Number(d.height)
+  })
+
+  const svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 500)
+
+  const rect = svg.selectAll('rect')
+    .data(data)
+
+  rect.enter().append('rect')
+    .attr('height', (d) => d.height)
+    .attr('width', 30)
+    .attr('fill', 'gray')
+    .attr('x', (d, i) => (i * 30) + (i * 10))
+    .attr('y', 0)
+}).catch(error => {
+  console.log(error)
+})
